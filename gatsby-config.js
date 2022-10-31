9eb15a1ec19c5e12f2e6395f08593fd3acba2c71
Eva Kuhejdová <eva.kuhejdova@seznam.cz>
2022-10-31 20:41:19 +0100

    Update Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,12 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-jodie/gatsby-config.js
-    siteTitle: `Jodie`,
-    siteTitleAlt: `Jodie - Gatsby Starter Portfolio`,
-    siteHeadline: `Jodie - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://jodie.lekoarts.de`,
+    siteTitle: `Eva Kuhejdová`,
+    siteTitleAlt: `Eva Kuhejdová - Portfolio`,
+    siteHeadline: `Eva Kuhejdová - Portfolio`,
+    siteUrl: `https://kuhejdova.cz`,
     siteDescription: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
   },
   plugins: [
     {
